a2cfebb1c8975f80324331e383f58ebc46dd5474
Daniel Lauzon <daniel.lauzon@gmail.com>
2011-10-27 14:11:18 -0400

    go back

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
-## rona-time Time sheet parser
-When it's up, you can find it [here](http://rona-time.cloudfoundry.com)
+## im-w Mongo backed metric tracker (weight)
+When it's up, you can find it [here](http://im-w.cloudfoundry.com)
+Started from example at
+  [cloudfoundry_node_mongodb](https://github.com/gatesvp/cloudfoundry_node_mongodb.git)
 
-    # get dependencies - not yet necessary
+    # get dependencies
     npm install
     
-    vmc target api.cloudfoundry.com
-    
     # if not yet created...
-    vmc push rona-time
+    vmc push im-w
+    # 5-add redis - ax-rq-redis
+    
     # to push an update
-    vmc update rona-time
+    vmc update im-w
 
+
+## Seeding initial data
+Couls use [xml2json](https://github.com/buglabs/node-xml2json), but try [plist](https://github.com/TooTallNate/node-plist) which uses sax npm module.
+
+    # convert observationdata.xml to observationdata.json
+    node loadobs.js
+    
+    # git diff to confirm, git commit to backup!
--- app.js
+++ app.js
@@ -1,86 +1,68 @@
-/*
-  refs: 
-  http://www.quora.com/What-is-the-best-way-to-read-a-file-line-by-line-in-node-js
-  http://blog.jaeckel.com/2010/03/i-tried-to-find-example-on-using-node.html
-  
-  iconv -f ISO-8859-1 -t UTF-8 TEST.txt >TEST.utf8.txt 
-  
-*/
-/* Http Variables */
+if(process.env.VCAP_SERVICES){
+  var env = JSON.parse(process.env.VCAP_SERVICES);
+  var mongo = env['mongodb-1.8'][0]['credentials'];
+}
+else{
+  var mongo = {
+    "hostname":"localhost",
+    "port":27017,
+    "username":"",
+    "password":"",
+    "name":"",
+    "db":""
+  }
+}
 
+var generate_mongo_url = function(obj){
+  obj.hostname = (obj.hostname || 'localhost');
+  obj.port = (obj.port || 27017);
+  obj.db = (obj.db || 'test');
+
+  if(obj.username && obj.password){
+    return "mongodb://" + obj.username + ":" + obj.password + "@" + obj.hostname + ":" + obj.port + "/" + obj.db + "?auto_reconnect=true";
+  }
+  else{
+    return "mongodb://" + obj.hostname + ":" + obj.port + "/" + obj.db;
+  }
+}
+
+var mongourl = generate_mongo_url(mongo);
+
+/* Http Variables */
 var port = (process.env.VMC_APP_PORT || 3000);
 var host = (process.env.VCAP_APP_HOST || '0.0.0.0'|| 'localhost');
 var http = require('http');
 
-/*
-1016         BEAUMIER             CLAUDE           *                 7.00  16.00                    12.00  21.00     8.00  17.00     7.00  16.00     7.00  16.00                      40.00            
-*/
+var record_visit = function(req, res){
+  /* Connect to the DB and auth */
+  require('mongodb').connect(mongourl, function(err, conn){
+    conn.collection('ips', function(err, coll){
+      /* Simple object to insert: ip address and date */
+      object_to_insert = { 'ip': req.connection.remoteAddress, 'ts': new Date() };
 
-function toMinutes(hdotm){
-    var hm=hdotm.split('.');
-    return 60*hm[0]+1*hm[1]
-}
-function dayEntry(pair){
-    pairRE=/(\d{1,3}\.\d\d)\s+(\d{1,3}\.\d\d)/g;
-    var m =  pairRE.exec(pair);
-    if (m){
-        //console.log(m);
-        return {debut:m[1],fin:m[2],dur:toMinutes(m[2])-toMinutes(m[1])};
-    } else return {dur:0};
-}
-function parseTimeSheet(){
-    var sheet={
-        produced:'Date inconnue',
-        entries:[]
-    };
-    var allText = fs.readFileSync(path.join(__dirname, 'TEST.utf8.txt'), 'utf8'); //.toString()
-    var lines = allText.split('\n');
-    var dataRE = /(\d[\d\s]{12})([A-Z][\w\s]{20})([A-Z][\w\s]{13})([\*\s]{19})([\d\.\s]{13})\s{3}([\d\.\s]{13})\s{3}([\d\.\s]{13})\s{3}([\d\.\s]{13})\s{3}([\d\.\s]{13})\s{3}([\d\.\s]{13})\s{3}([\d\.\s]{13})\s{1}([\d\.\s]{10})/;
-    var stampRE = /Date de production.*(\d\d\/\d\d\/\d\d\d\d)/;
-    lines.forEach(function (line) {
-        var m =  dataRE.exec(line);
-        if (m){
-            //console.log(m);
-            var rec = {
-                no: m[1],
-                last: m[2],
-                first: m[3],
-                //star: m[4],
-                jours:[
-                  dayEntry(m[5]),
-                  dayEntry(m[6]),
-                  dayEntry(m[7]),
-                  dayEntry(m[8]),
-                  dayEntry(m[9]),
-                  dayEntry(m[10]),
-                  dayEntry(m[11]),
-                ],
-                total: m[12],
-                totalMinutes:toMinutes(m[12]),
-                totalCalc:0,
-                inputLine:line
-            }
-            rec.jours.forEach(function (entry) {
-                rec.totalCalc+=entry.dur;
-            });
-            //console.log(rec);
-            sheet.entries.push(rec);
-            //res.write(line+'\n');
+      /* Insert the object then print in response */
+      /* Note the _id has been created */
+      coll.insert( object_to_insert, {safe:true}, function(err){
+        res.writeHead(200, {'Content-Type': 'text/plain'});
+        res.write(JSON.stringify(object_to_insert)+'\n\n');
+        if (!process.env.VCAP_SERVICES){
+            res.write(JSON.stringify(mongourl)+'\n\n');
+            res.write(JSON.stringify(mongo)+'\n\n');
+            res.write(require('util').inspect(process.env, false, null)+'\n');                        
         }
-        if (stampRE.test(line)){
-            //console.log(stampRE.exec(line));
-            sheet.produced=stampRE.exec(line)[1];
-        }
+
+        var fs = require('fs'), path=require('path');
+        var obsjson = fs.readFileSync(path.join(__dirname, 'observationdata.json'), 'utf8');
+        res.write(obsjson+'\n');
+
+        res.end('\n');
+      });
     });
-    return sheet;
+  });
 }
 
 http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    var sheet = parseTimeSheet();
-    res.write(JSON.stringify(sheet,null,4));
-    res.end('\n');
+  record_visit(req, res);
 }).listen(port, host);
-//console.log('open http://'+host+':'+port);
 
-parseTimeSheet();
+console.log('http://'+host+':'+port+'/');
