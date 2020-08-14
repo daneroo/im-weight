c819a0e09c8d61bf4761b6474f63e2386b471a84
Daniel Lauzon <daniel.lauzon@gmail.com>
2020-08-14 16:13:59 -0400

    vRatio to .707 radial gradianet shrink 30,50

--- nextjs/components/DragSVG.js
+++ nextjs/components/DragSVG.js
@@ -110,7 +110,7 @@ export const AnchorZoom = ({ style, onDrag, onDelta }) => {
 }
 
 export const ArcSlider = ({ style, onDrag, onDelta }) => {
-  const vRatio = 0.5
+  const vRatio = 0.707
   const { theme: { colors: { primary, text } } } = useTheme()
 
   // compress incoming by vRatio (initial and xy)
